de1ff7c65fe6ded3e5cdf88aca91e19675910708
rubik <lifesonrubik@gmail.com>
2023-07-07 09:47:59 -0600

    switch to chartjs

--- README.md
+++ README.md
@@ -1,13 +1,16 @@
 # p5XYgraphing
-2d xy graphing using p5js
+2d xy graphing using Chartjs
 
 
 # Project current state
-This project currently contains the backend to solve and graph a TSP problem (Traveling Salesman Problem) using a genetic algorithm and matplotlib. The p5js frontend component is in development.
+This project currently contains the backend to solve and graph a TSP problem (Traveling Salesman Problem) using a genetic algorithm (Python backend) and Chartjs for the graphing component (frontend). In previous commits the project used p5js for the front end, but for the mean time it is way easier to set it up with Chartjs. The output process (backend) also saves local files with images created with matplotlib, and csv files with rather the points data, the distances and permutations or the best route created.
+
+**Why permutations and not combinations?**
+The objective is to implement this project to handle transportation and logistic operations, so in real scenario, the distance for a vehicle to move from A to B most often times won’t be the same as from B to A, why? Because of U-turns, detours, bridges, etc.
 
 
-## Usage
-The main.py in the main branch calls the necessary functions to perform an input-output process to solve the TSP process with pre-acquired data.
+## Backend usage
+The main.py in the main branch calls the necessary functions to perform an input-output process to solve the TSP problem with pre-acquired data.
 
 The model.py file contains functions to create (x, y) data points from scratch that can be stored as a .csv file or in a relational database for later use.
 
@@ -17,6 +20,12 @@ An entire process can be completed using only .csv files as an input which is ha
 **Notice:** The current SQL engine tested for this project is sqlite3 
 
 The model.py file also contains functions to update and handle the SQL database if needed.
+
+## Frontend
+No need to install any npm packages since it uses jsdelivr:
+><script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+The index.html file can be opened without a server, it will ask for the “best route” csv file.
 
 
 # Python
--- src/main.js
+++ src/main.js
@@ -1,90 +1,137 @@
 "use strict";
 
 let to_array = [];
+let myChart;
 
-function loadFile (event) {
-    var csvfile = event.target.files[0];
+function loadFile(event) {
+  const csvfile = event.target.files[0];
 
-    if (csvfile) {
-        var ta_original   = document.getElementById("original");
-        var only_zeros    = true;
-        var reader        = new FileReader();
+  if (csvfile) {
+    var ta_original = document.getElementById("original");
+    var only_zeros = true;
+    var reader = new FileReader();
 
-        // Called after starting a read operation to initialize the textareas and the matrix
-        reader.onloadstart = function () {
-            ta_original.value   = "";
+    reader.onloadstart = function () {
+      ta_original.value = "";
+    };
+
+    reader.onload = function () {
+      var lines = reader.result.split(reader.result.indexOf("\r") > 0 ? "\r\n" : "\n");
+      var delim = lines[0].indexOf(",") == -1 ? " " : ",";
+
+      document.getElementById("head-text-div").innerHTML = lines[0];
+
+      var matrix = new Array(lines.length - 1);
+
+      for (var row = 1; row < lines.length; row++) {
+        var values = lines[row].split(delim);
+
+        matrix[row - 1] = new Array(values.length);
+        for (var col = 0; col < values.length; col++) {
+          matrix[row - 1][col] = parseInt(values[col]);
+          ta_original.value += values[col];
+          ta_original.value += col + 1 < values.length ? delim : "";
+
+          if (only_zeros && matrix[row - 1][col] != 0) {
+            only_zeros = false;
+          }
         }
+        ta_original.value += row + 1 < lines.length ? "\n" : "";
+      }
+    };
 
-        // Called when a read operation successfully completes. It separates the CSV headings
-        // from the values and process them. Everything gets stored in the textareas and the matrix.
-        reader.onload  = function () {
-            var lines  = reader.result.split(reader.result.indexOf("\r") > 0 ? "\r\n" : "\n");
-            var delim  = lines[0].indexOf(",") == -1 ? " " : ",";
-
-            // Get & set the headings
-            document.getElementById("head-text-div").innerHTML = lines[0];
-
-            // Create a dynamic two-dimensional array (matrix) and store all values
-            var matrix    = new Array(lines.length - 1);
-
-            for (var row=1; row<lines.length; row++) {
-                var values  = lines[row].split(delim);
-
-                matrix[row-1] = new Array(values.length);
-                for (var col=0; col<values.length; col++) {
-                    matrix[row-1][col] = parseInt(values[col]);
-                    ta_original.value += values[col];
-                    ta_original.value += col+1 < values.length ? delim : "";
-
-                    // There should be at least one number different than zero
-                    if (only_zeros && matrix[row-1][col] != 0) {
-                        only_zeros = false;
-                    }
-                }
-                ta_original.value += row+1 < lines.length ? "\n" : "";
-            }
+    reader.onloadend = function () {
+      if (reader.error) {
+        alert("The file was not loaded successfully");
+        console.error("Unsuccessful load of " + csvfile.name + ": " + reader.error.message);
+      } else {
+        to_array = ta_original.value.split("\n");
+        for (var i = 0; i < to_array.length; i++) {
+          to_array[i] = to_array[i].split(",");
         }
+        to_array.pop();
+        var html = ta_original.value.replace(/\n/g, "<br>");
+        document.getElementById("text-div").innerHTML = html;
 
-        // Called after a read completes (either successfully or unsuccessfully)
-        reader.onloadend = function() {
-            if (reader.error) {
-                alert("The file was not loaded successfully. Please try again.");
-                console.error("Unsuccessful load of " + csvfile.name + ": " + reader.error.message);
-            }
-            else {
-                // Get the textarea's value and create the html content of the text-div
-                // convert each row in the ta_original.value to an array, each row should be its own inner array inside the outer array
-                // this array will be used to draw points and lines using p5js liibrary
-                to_array = ta_original.value.split("\n");
-                for (var i = 0; i < to_array.length; i++) {
-                    to_array[i] = to_array[i].split(",");
-                }
-                // get rid of the last empty array
-                to_array.pop();
-                console.log(" to_array = " + typeof to_array)
-                console.log(to_array)
-                console.log(" ta_original.value = " + typeof ta_original.value)
-                console.log(ta_original.value)
-                var html = ta_original.value.replace(/\n/g,"<br>");
-                document.getElementById("text-div").innerHTML = html;
-
-                // Show the elements
-                var hidden = document.getElementsByClassName("csv-details");
-                if (hidden.length > 0) {
-                    hidden[0].className = "";
-                }
-            }
+        var hidden = document.getElementsByClassName("csv-details");
+        if (hidden.length > 0) {
+          hidden[0].className = "";
         }
+        startChart(to_array);
+      }
+    };
 
-        // Called when there is an error during the load
-        reader.onerror = function () {
-            console.error("Error while loading " + csvfile.name + ": " + reader.error.message);
-        }
+    reader.onerror = function () {
+      console.error("Error while loading " + csvfile.name + ": " + reader.error.message);
+    };
 
-        // Read from blob as a string and the result will be stored on this.result after the 'load' event fires
-        reader.readAsText(csvfile);
-    }
-    else {
-        alert("Oops! Something unexpected happened... Please try again.");
-    }
+    reader.readAsText(csvfile);
+  } else {
+    alert("Unexpected error");
+  }
+}
+
+function startChart(fileContent) {
+  const xValues = [];
+  const yValues = [];
+
+  for (let i = 0; i < fileContent.length; i++) {
+    xValues.push(fileContent[i][0]);
+    yValues.push(fileContent[i][1]);
+  }
+
+  const chartConfig = {
+    type: "line",
+    data: {
+      datasets: [
+        {
+          label: "XY Data",
+          data: xValues.map((x, i) => ({ x: x, y: yValues[i] })),
+          borderColor: "#ffeecc",
+          backgroundColor: "#ffeecc",
+          pointRadius: 5,
+          pointBackgroundColor: "#ffcc99",
+          lineTension: 0.2,
+          fill: false,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        x: {
+          type: "linear",
+          position: "bottom",
+        },
+        y: {
+          type: "linear",
+          position: "left",
+        },
+      },
+      aspectRatio: 1,
+    },
+  };
+
+  const chartContainer = document.getElementById("chartContainer");
+  const canvas = document.getElementById("myChart");
+
+  if (myChart) {
+    myChart.destroy();
+  }
+
+  myChart = new Chart(canvas.getContext("2d"), chartConfig);
+
+  function updateChartSize() {
+    const chartContainer = document.getElementById("chartContainer");
+    const canvas = document.getElementById("myChart");
+  
+    const containerWidth = chartContainer.clientWidth;
+    const containerHeight = containerWidth;
+  
+    canvas.width = containerWidth;
+    canvas.height = containerHeight;
+  
+    myChart.resize();
+  }
+
+  window.addEventListener("resize", updateChartSize);
 }
