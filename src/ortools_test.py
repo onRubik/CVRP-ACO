1f5a4045a079ead0b04a93fdca6ee9fd87238efa
onRubik <lifesonrubik@gmail.com>
2023-11-06 17:56:33 -0600

    ortools tests

--- src/ortools_test.py
+++ src/ortools_test.py
@@ -1,63 +1,70 @@
 from ortools.constraint_solver import routing_enums_pb2
 from ortools.constraint_solver import pywrapcp
+from tqdm import tqdm
 
 def create_data_model():
-    """Stores the data for the problem."""
+    """Create data for the problem."""
     data = {}
     data['distance_matrix'] = [
-        [0, 5, 7, 10],
-        [5, 0, 10, 6],
-        [7, 10, 0, 8],
-        [10, 6, 8, 0]
-    ]  # Replace this matrix with your actual distances between locations.
-
-    data['num_vehicles'] = 1
+        [0, 10, 15, 20, 25, 30, 35, 40],
+        [10, 0, 35, 25, 20, 40, 45, 50],
+        [15, 35, 0, 30, 50, 10, 45, 55],
+        [20, 25, 30, 0, 10, 15, 20, 25],
+        [25, 20, 50, 10, 0, 30, 35, 40],
+        [30, 40, 10, 15, 30, 0, 40, 45],
+        [35, 45, 45, 20, 35, 40, 0, 55],
+        [40, 50, 55, 25, 40, 45, 55, 0]
+    ]
+    data['num_vehicles'] = 3
+    data['vehicle_capacities'] = [30, 40, 50]
     data['depot'] = 0
     return data
 
-def print_solution(manager, routing, solution):
+def print_solution(data, manager, routing, solution):
     """Prints the solution on the console."""
-    print('Objective: {}'.format(solution.ObjectiveValue()))
-    index = routing.Start(0)
-    plan_output = 'Route:\n'
-    route_distance = 0
-    while not routing.IsEnd(index):
-        plan_output += ' {} ->'.format(manager.IndexToNode(index))
-        previous_index = index
-        index = solution.Value(routing.NextVar(index))
-        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
-    plan_output += ' {}\n'.format(manager.IndexToNode(index))
-    print(plan_output)
-    print('Route distance: {} units'.format(route_distance))
+    for vehicle_id in range(data['num_vehicles']):
+        index = routing.Start(vehicle_id)
+        plan_output = f'Route for Vehicle {vehicle_id}:\n'
+        route_distance = 0
+        while not routing.IsEnd(index):
+            node_index = manager.IndexToNode(index)
+            plan_output += f' {node_index} ->'
+            previous_index = index
+            index = solution.Value(routing.NextVar(index))
+            route_distance += routing.GetArcCostForVehicle(previous_index, index, vehicle_id)
+        plan_output += f' {manager.IndexToNode(index)}\n'
+        plan_output += f'Route distance for Vehicle {vehicle_id}: {route_distance} units\n'
+        print(plan_output)
 
 def main():
-    # Instantiate the data problem.
     data = create_data_model()
 
-    # Create the routing index manager.
     manager = pywrapcp.RoutingIndexManager(len(data['distance_matrix']), data['num_vehicles'], data['depot'])
-
-    # Create Routing Model.
     routing = pywrapcp.RoutingModel(manager)
 
-    # Create and register a transit callback.
-    def distance_callback(from_index, to_index):
-        """Returns the distance between the two nodes."""
-        from_node = manager.IndexToNode(from_index)
-        to_node = manager.IndexToNode(to_index)
-        return data['distance_matrix'][from_node][to_node]
+    # Create your own distance and demand callback functions
 
-    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
-    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
-
-    # Set 10 seconds as the maximum time allowed to search for solutions.
     search_parameters = pywrapcp.DefaultRoutingSearchParameters()
-    search_parameters.time_limit.seconds = 10
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PARALLEL_CHEAPEST_INSERTION
+    )
 
-    # Solve the problem.
-    solution = routing.SolveWithParameters(search_parameters)
+    solution = None
+    with tqdm(total=100) as pbar:
+        def update_progress(percent):
+            nonlocal pbar
+            pbar.n = percent
+            pbar.refresh()
+
+        def progress_tracker():
+            nonlocal solution
+            solution = routing.SolveWithParameters(search_parameters)
+            update_progress(100)
+
+        progress_tracker()
+
     if solution:
-        print_solution(manager, routing, solution)
+        print_solution(data, manager, routing, solution)
 
 if __name__ == '__main__':
     main()
