1c7cf9ff113986d31aeb70fefc337dca4b82094a
onRubik <lifesonrubik@gmail.com>
2024-01-28 11:53:28 -0600

    post method

--- website/static/index.js
+++ website/static/index.js
@@ -1,1 +1,19 @@
+function displayFlashMessages() {
+    var post_request = sessionStorage.getItem("post_request");
+    
+    if (!post_request) {
+        var flashMessages = document.querySelectorAll(".flash-message");
+        if (flashMessages && flashMessages.length > 0) {
+            flashMessages.forEach(function (messageElement) {
+                var category = messageElement.getAttribute("data-category");
+                var content = messageElement.textContent;
 
+                alert("Category: " + category + "\nContent: " + content);
+
+                sessionStorage.setItem("post_request", true);
+            });
+        }
+    }
+}
+
+displayFlashMessages();
--- website/views.py
+++ website/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, request, redirect, url_for, Blueprint, jsonify, flash
+from flask import render_template, request, redirect, url_for, Blueprint, jsonify, flash, session
 from .clustering import ClusteringService
 from .tsp import TspService
 from .load_points import load_points  
@@ -14,18 +14,24 @@ ALLOWED_EXTENSIONS = set(['csv'])
 
 @views.route('/', methods=['GET', 'POST'])
 def home():
-    if request.method =='POST':
+    print(request.method)
+    post_request = session.get('message_displayed', False)
+    print(post_request)
+
+    if request.method =='POST' and post_request==False:
         file = request.files['file']
         if file and allowed_file(file.filename):
             try:
                 df = pd.read_csv(file, header=None)
                 dist_v = df[0].unique()
                 dist_db_v = db.session.query(DVRPSet.dvrp_id).distinct().all()
-                print(dist_db_v)
                 dist_db_v = [i[0] for i in dist_db_v]
                 if dist_v in dist_db_v:
-                    error_message = 'dvrp_id exists in dvrp_set table'
-                    flash(error_message, 'error')
+                    print('it went in')
+                    message = 'dvrp_id exists in dvrp_set table'
+                    flash(message, category='error')
+                    session['post_request']=True
+                    print(session)
                 else:
                     for index, row in df.iterrows():
                         dvrp_set = DVRPSet(
@@ -37,6 +43,9 @@ def home():
                         db.session.add(dvrp_set)
 
                     db.session.commit()
+                    message = 'set added to dvrp_set table'
+                    flash(message, category='success')
+                    session.pop('post_request', None)
             except Exception as e:
                 db.session.rollback()
 
